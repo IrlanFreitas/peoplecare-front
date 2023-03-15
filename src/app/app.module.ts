aea7748c886fc57bc4558c29977287f6db9c7be5
Irlan Freitas <irlan.freitas@hotmail.com>
2023-03-15 14:59:52 -0300

    feat: desenvolvimento da estrutura para o projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { FooterComponent } from './components/footer/footer.component';
+import { MainComponent } from './components/main/main.component';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import { CardComponent } from './components/card/card.component';
+import { ListComponent } from './components/patients/list/list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainComponent,
+    CardComponent,
+    ListComponent
   ],
   imports: [
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
